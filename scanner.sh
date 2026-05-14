e2d1b69f424995c7d92dac00affc0f5d671a8e2e
NotDerrickk <roro.neveu@gmail.com>
2026-05-14 18:36:57 -0400

    add first step : test the availability of an ip

--- scanner.sh
+++ scanner.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 # nmap_diy 
 
-echo "hello world"
+echo "Entrez un adresse ip : " 
+read ip 
+echo "ip testée : $ip"
+ping -c 1 -W 1 $ip > /dev/null
+
+if [ $? -eq 0 ]; then  
+    echo "Cible $ip est en accessible"
+else 
+    echo "Cible $ip est inaccessbile"
+fi
