f4344f0ac972b618cf4ef2b2acc8835f7b56ea25
NotDerrickk <roro.neveu@gmail.com>
2026-05-15 13:10:32 -0400

    Add ip check range

--- scanner.sh
+++ scanner.sh
@@ -13,14 +13,23 @@ while true; do
         1)
             read -p "Entrez une adresse IP : " ip
             echo "ip testée : $ip"
-            if ping -c 1 -W 1 "$ip" > /dev/null 2>&1; then
-                echo "Cible $ip est accessible"
-            else
-                echo "Cible $ip est inaccessible"
+
+            if ping -c 1 -W 1 $ip > /dev/null 2>&1; then
+                echo "$ip addresse IP accessible"
             fi
             ;;
         2)
-            echo "Option 2: Trouver les machines actives d'un réseau local"
+            read -p "Entrez une plage d'adresses IP (ex:192.168.1.1/24) : " subnet
+            echo "Scan en cours pour le réseau $subnet..."
+            ip=$(echo "$subnet" | cut -d'/' -f1)
+            mask=$(echo "$subnet" | cut -d'/' -f2)
+
+            base_ip=$(echo "$ip" | cut -d'.' -f1-3)
+            for i in $(seq 1 254); do 
+                if ping -c 1 -W 1 $base_ip.$i > /dev/null 2>&1; then
+                    echo "$base_ip.$i addresse IP accessible"
+                fi
+            done
             ;;
         q|Q)
             echo "Au revoir."
